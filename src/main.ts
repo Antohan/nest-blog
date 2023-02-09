2da77726fbe6dece4d1f284c4ea70e763493b4e0
Anton Sukhodolets <theAntohan@yandex.ru>
2023-02-09 21:10:10 +0700

    feat: init open api specification

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app/app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule } from '@nestjs/swagger';
+import { resolve } from 'path';
+import { readFile } from 'fs/promises';
+import * as yaml from 'yaml';
+
+import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
+  const document = yaml.parse(await readFile(resolve(__dirname, '..', 'openapi.yaml'), 'utf8'));
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
