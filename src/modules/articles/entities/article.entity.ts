b16e483f1de814a7778b73117b562a06d6904e58
Anton Sukhodolets <theAntohan@yandex.ru>
2023-02-25 13:24:19 +0700

    feat: :card_file_box: Add database connection

--- src/modules/articles/entities/article.entity.ts
+++ src/modules/articles/entities/article.entity.ts
@@ -1,5 +1,24 @@
-export interface Article {
-  readonly id: string;
-  readonly title: string;
-  readonly content: string;
+import { Column, DataType, Model, Table } from 'sequelize-typescript';
+
+@Table
+export class Article extends Model<Article> {
+  @Column({
+    type: DataType.UUID,
+    defaultValue: DataType.UUIDV4(),
+    allowNull: false,
+    primaryKey: true,
+  })
+  id: string;
+
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  title: string;
+
+  @Column({
+    type: DataType.TEXT,
+    allowNull: false,
+  })
+  content: string;
 }
